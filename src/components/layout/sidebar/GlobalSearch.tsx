9324134001a36cac6aaa5df313467aeecae36d3f
Jawad Nazir <jawad.nazir@rubixx.co.uk>
2025-02-24 13:40:01 +0000

    fix build error, enxt form not recognised for some reason on vercel deployments

--- src/components/layout/sidebar/GlobalSearch.tsx
+++ src/components/layout/sidebar/GlobalSearch.tsx
@@ -1,11 +1,9 @@
-import Form from "next/Form"
-
 export default function GlobalSearch(){
     return(
-        <Form action="/search" className="max-w-[800px] w-full">
+        <div  className="max-w-[800px] w-full">
         {/* On submission, the input value will be appended to 
             the URL, e.g. /search?query=abc */}
         <input name="query"  placeholder="Search" className="px-3 py-[.3rem] w-full rounded-lg"/>
-    </Form>
+    </div>
     )
 }
